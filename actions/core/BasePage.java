6e301e73f363aebdc6f92bd48e484492f7107a7b
nguyentra <ngtra21@gmail.com>
2026-04-23 10:23:55 +0700

    u

--- actions/core/BasePage.java
+++ actions/core/BasePage.java
@@ -1,4 +1,12 @@
 package core;
 
-public class BasePage {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public abstract class BasePage {
+    WebDriver driver;
+    public WebElement getWebElement (String locator){
+        return driver.findElement(By.cssSelector(locator));
+    }
 }
